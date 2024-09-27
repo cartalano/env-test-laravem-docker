4514e71704cd96f4dedd927cd663cfd7e95bde13
Nicolas Catalano <nicolascatalano1@gmail.com>
2024-09-27 16:27:19 +0200

    Suite du tuto laravel

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,11 +1,34 @@
 <?php
-
+use illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-route::get('/blog', function () {
-    return "Bonjour";
+route::get('/blog', function (Request $request) {
+    return [
+        'article' => 'Article1',
+        'name' => $request->url(),
+
+    ];
 });
+
+route::get('/blog/{slug}-{id}', function(string $slug, string $id) {
+    return [
+        "slug" => $slug,
+        'id' => $id
+    ];
+})->where([
+    'id' => '[0-9]+',
+    'slug' => '[a-z0-9\-]+'
+]);
+
+/*
+$post = new \app\Models\Post();
+$post->title = 'Mon premier article';
+$post->slug = 'mon-premier-article';
+$post->content = 'Mon contenu';
+$post->save();
+
+return $post; */
